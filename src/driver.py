ee742da3e3033c7eb57c896b5dc8ee066510077e
ericrqs <eric.r@quali.com>
2016-09-07 17:29:35 -0400

    Restore takes folder name and generates filename from model and resource name

--- src/driver.py
+++ src/driver.py
@@ -133,6 +133,10 @@ class GigamonDriver (ResourceDriverInterface):
 
         running_saved = 'running' if configuration_type.lower() == 'running' else 'saved'
 
+        path = '%s/%s_%s.txt' % (path if not path.endswith('/') else path[0:-1],
+                                 context.resource.name.replace(' ', '-'),
+                                 context.resource.model.replace(' ', '-'))
+
         self._ssh_command('configure terminal', '[^[#]# ')
         try:
             if '://' in path:
