23effd379ec79a0a0be772f22bc743a3dd2a8588
ericrqs <eric.r@quali.com>
2016-09-12 17:56:06 -0400

    fix x1b

--- src/driver.py
+++ src/driver.py
@@ -76,7 +76,7 @@ class GigamonDriver (ResourceDriverInterface):
             if r:
                 rv += r
             if rv:
-                t = re.sub(r'\x1b\[\d+m', '', rv).replace(r'\x1b', '').replace('\b', '')
+                t = re.sub(r'\x1b\[\d+m', '', rv).replace('\x1b', '').replace('\b', '')
             else:
                 t = ''
             if not r or len(re.findall(prompt_regex, t)) > 0:
